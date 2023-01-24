2d58644c27ad62d664058264c0e56480a9f94f2f
trofimovpv <102983027+trofimovpv@users.noreply.github.com>
2023-01-24 15:48:54 +0300

    Update CashbackHackServiceTest.java

--- src/main/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/main/java/ru/netology/service/CashbackHackServiceTest.java
@@ -14,7 +14,7 @@ public class CashbackHackServiceTest {
         int actual = 0;
         int expected = service.remain(1000);
 
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
@@ -24,7 +24,7 @@ public class CashbackHackServiceTest {
         int actual = 1;
         int expected = service.remain(999);
 
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
@@ -33,6 +33,6 @@ public class CashbackHackServiceTest {
         int actual = 999;
         int expected = service.remain(1001);
 
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
 }
